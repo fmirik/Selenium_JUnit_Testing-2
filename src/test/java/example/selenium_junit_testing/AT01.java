2b70fbcf23aeaaaeebcedd8431b1c276bb4bbda2
fmirik <fatihmirik@gmail.com>
2023-04-29 02:09:11 +0300

    first commit

--- src/test/java/example/selenium_junit_testing/AT01.java
+++ src/test/java/example/selenium_junit_testing/AT01.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 
 import java.io.File;
 import java.time.Duration;
